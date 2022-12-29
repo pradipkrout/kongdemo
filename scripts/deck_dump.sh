c11e93ffd159e7a649d32708dcc8948efe85144a
pradipkrout <103028544+pradipkrout@users.noreply.github.com>
2022-12-30 00:05:50 +0530

    Update deck_dump.sh

--- scripts/deck_dump.sh
+++ scripts/deck_dump.sh
@@ -1,15 +1,26 @@
- #!/bin/bash
- [ $? -eq 0 ] || exit
-while getopts "U:P:J:I:" flag
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+ echo "Please check arguments detail"
+ exit
+else
+ while getopts "t:a:r:s:" flag
     do
              case "${flag}" in
-                    U) TEST_USER=${OPTARG};;
-                    P) TEST_PWD=${OPTARG};;
-                    J) TEST_JOBID=${OPTARG};;
-                    I) TEST_PROJECTID=${OPTARG};;
+                    t) KONNECT_TOKEN=${OPTARG};;
+                    a) KONNECT_ADDR=${OPTARG};;
+                    r) KONNECT_RUNTIME_GROUP=${OPTARG};;
+                    s) SELECT_TAGS=${OPTARG};;
              esac
     done
-    echo "USER: $TEST_USER";
-    echo "PWD: $TEST_PWD";
-    echo "JOBID: $TEST_JOBID";
-    echo "PROJECTID: $TEST_PROJECTID";
+    echo "KONNECT_TOKEN: $KONNECT_TOKEN";
+    echo "KONNECT_ADDR: $KONNECT_ADDR";
+    echo "KONNECT_RUNTIME_GROUP: $KONNECT_RUNTIME_GROUP";
+    echo "SELECT_TAGS: $SELECT_TAGS";
+    deck ping --konnect-runtime-group-name $KONNECT_RUNTIME_GROUP --konnect-token $KONNECT_TOKEN --konnect-addr $KONNECT_ADDR  --tls-skip-verify
+    mkdir results
+    datenow=$(date "+%F-%H-%M-%S")
+    echo "date now is $datenow"
+    deck dump --konnect-runtime-group-name $KONNECT_RUNTIME_GROUP --konnect-token $KONNECT_TOKEN --konnect-addr $KONNECT_ADDR --select-tag $SELECT_TAGS --yes --format yaml --output-file results/konnect-${datenow}.yaml --tls-skip-verify
+   
+fi
