0f1c269a94fc3e682713d02a9faafa2372280975
pradipkrout <103028544+pradipkrout@users.noreply.github.com>
2022-12-30 21:19:16 +0530

    Update action.sh

--- scripts/action.sh
+++ scripts/action.sh
@@ -2,6 +2,6 @@
 
 echo "exeuction of script $0 and $1"
 deck ping --konnect-runtime-group-name $1 --konnect-token $2 --tls-skip-verify > konnect_ping
-if expr `cat konnect_ping` : *"Successfully"* ; then
+if grep -q "ab" <<< "abc" ; then
 echo "Inside if block"
 fi
