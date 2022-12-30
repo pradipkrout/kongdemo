bf252a20a20c0c285c11ef59f0b0ad7576fd970b
pradipkrout <103028544+pradipkrout@users.noreply.github.com>
2022-12-30 21:24:36 +0530

    Update action.sh

--- scripts/action.sh
+++ scripts/action.sh
@@ -2,6 +2,8 @@
 
 echo "exeuction of script $0 and $1"
 deck ping --konnect-runtime-group-name $1 --konnect-token $2 --tls-skip-verify > konnect_ping
-if [[ "abcd" == *"bc"* ]] ; then
+konnectcheck=`cat konnect_ping`
+echo $konnectcheck
+if [[ "$konnectcheck" == *"Successfully"* ]] ; then
 echo "Inside if block"
 fi
