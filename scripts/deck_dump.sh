a50c7d6918926d5ba759e6b3ff4d2e1e444e57a4
pradipkrout <103028544+pradipkrout@users.noreply.github.com>
2022-12-30 20:38:34 +0530

    Update deck_dump.sh

--- scripts/deck_dump.sh
+++ scripts/deck_dump.sh
@@ -22,7 +22,7 @@ else
     mkdir results;
     datenow=$(date "+%F-%H-%M-%S");
     echo "date now is $datenow";
-    deck dump --konnect-runtime-group-name $1 --konnect-token $2 --konnect-addr $3 --select-tag $4 --yes --format yaml --output-file results/konnect-${datenow}.yaml --tls-skip-verify
-    cat results/konnect-${datenow}.yaml
+    deck dump --konnect-runtime-group-name $1 --konnect-token $2 --konnect-addr $3 --select-tag $4 --yes --format yaml --output-file results/konnect-${datenow}.yaml --tls-skip-verify;
+    cat results/konnect-${datenow}.yaml;
    fi
 fi
