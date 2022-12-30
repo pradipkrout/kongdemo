3a7be2a1c3b4d0abf893ff75829aae02044d1b91
pradipkrout <103028544+pradipkrout@users.noreply.github.com>
2022-12-30 21:10:39 +0530

    Update action.sh

--- scripts/action.sh
+++ scripts/action.sh
@@ -2,6 +2,6 @@
 
 echo "exeuction of script $0 and $1"
 deck ping --konnect-runtime-group-name $1 --konnect-token $2 --tls-skip-verify > konnect_ping
-if [[ `cat konnect_ping` == *"Successfully"* ]]; then
+if expr `cat konnect_ping` : *"Successfully"* ; then
 echo "Inside if block"
 fi
