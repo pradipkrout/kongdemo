fd3eb917d7d79750373e018a23bf88cda7c0afc3
pradipkrout <103028544+pradipkrout@users.noreply.github.com>
2022-12-30 20:45:44 +0530

    Update action.sh

--- scripts/action.sh
+++ scripts/action.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "exeuction of script $0 and $1"
-deck $1 --konnect-runtime-group-name $2 $3 --tls-skip-verify
+deck $1 --konnect-runtime-group-name $2 --konnect-token $3 --tls-skip-verify
