d9a3c378b57f10665ed212d33e1e02ecfe9335e9
pradipkrout <103028544+pradipkrout@users.noreply.github.com>
2022-12-30 23:03:50 +0530

    Update deck_sync.sh

--- scripts/deck_sync.sh
+++ scripts/deck_sync.sh
@@ -1,28 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $# -eq 0 ]; then
- echo "Please check arguments detail"
- exit
-else
- #while getopts "t:a:r:s:" flag
- #   do
- #            case "${flag}" in
- #                   t) KONNECT_TOKEN=${OPTARG};;
- #                   a) KONNECT_ADDR=${OPTARG};;
- #                   r) KONNECT_RUNTIME_GROUP=${OPTARG};;
- #                   s) SELECT_TAGS=${OPTARG};;
- #            esac
- #   done
-    echo "KONNECT_TOKEN: $KONNECT_TOKEN";
-    echo "KONNECT_ADDR: $KONNECT_ADDR";
-    echo "KONNECT_RUNTIME_GROUP: $KONNECT_RUNTIME_GROUP";
-    echo "SELECT_TAGS: $SELECT_TAGS";
-    deck ping --konnect-runtime-group-name $1 --konnect-token $2 --konnect-addr $3  --tls-skip-verify > konnect_ping;
-    if [[ `cat konnect_ping` == *"Successfully"* ]]; then
-    mkdir results;
-    datenow=$(date "+%F-%H-%M-%S");
-    echo "date now is $datenow";
-    deck dump --konnect-runtime-group-name $1 --konnect-token $2 --konnect-addr $3 --select-tag $4 --yes --format yaml --output-file results/konnect-${datenow}.yaml --tls-skip-verify;
-    cat results/konnect-${datenow}.yaml;
-   fi
-fi
+echo "exeuction of script $0 and $1"
+deck ping --konnect-runtime-group-name $1 --konnect-token $2 --tls-skip-verify > konnect_ping
+konnectcheck=`cat konnect_ping`
+echo $konnectcheck
+##if [[ "$konnectcheck" == *"Successfully"* ]] ; then
+##echo "Inside if block"
+##fi
+case $konnectcheck in
+
+  *"Successfully"*)
+    chmod 755 ./generate_yaml.py
+    python3 ./generate_yaml.py
+    deck validate --konnect-addr $3 --konnect-token $2 --konnect-runtime-group-name $1 -s $4/results/$5 > konnect_validate
+    ;;
+esac
+konnectvalidcheck=`cat konnect_validate`
+echo $konnectvalidcheck
+
+case $konnectvalidcheck in
+
+  *"Successfully"*)
+    deck diff --konnect-token $2 --konnect-addr $3 --konnect-runtime-group-name $1 -s $4/results/$5 > konnect_diff
+    deck sync --konnect-addr $3 --konnect-token $2 --konnect-runtime-group-name $1 -s $(System.DefaultWorkingDirectory)/results/${{ parameters.kong_file }}
+    ;;
+esac
