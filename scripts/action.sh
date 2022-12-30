f156e05579e53b1839a4dfcb018b38401711718f
pradipkrout <103028544+pradipkrout@users.noreply.github.com>
2022-12-30 22:10:19 +0530

    Update action.sh

--- scripts/action.sh
+++ scripts/action.sh
@@ -11,5 +11,10 @@ case $konnectcheck in
 
   *"Successfully"*)
     echo -n "It's there."
+     mkdir results
+    datenow=$(date "+%F-%H-%M-%S")
+    echo "date now is $datenow"
+    deck dump --konnect-runtime-group-name $1 --konnect-token $2 --konnect-addr $3 --select-tag $4 --yes --format yaml --output-file results/konnect-${datenow}.yaml --tls-skip-verify
+    cat results/konnect-${datenow}.yaml
     ;;
 esac
