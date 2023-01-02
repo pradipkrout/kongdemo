c5e880cc9d717a622968f1909e46155e1d3d52db
pradipkrout <103028544+pradipkrout@users.noreply.github.com>
2023-01-02 22:54:27 +0530

    Update deck_sync.sh

--- scripts/deck_sync.sh
+++ scripts/deck_sync.sh
@@ -13,7 +13,7 @@ case $konnectcheck in
     mkdir results
     datenow=$(date "+%F-%H-%M-%S")
     echo "date now is $datenow"
-    deck validate --konnect-runtime-group-name $1 --konnect-token $2 --konnect-addr $3 -s $4/results/$5 > konnect_validate
+    deck validate --konnect-runtime-group-name $1 --konnect-token $2 --konnect-addr $3 -s $4/$6/$5 > konnect_validate
     declvalidatechk=`cat konnect_ping`
     echo $declvalidatechk
     ;;
