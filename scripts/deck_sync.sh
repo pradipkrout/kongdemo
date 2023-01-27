c514df2483ce52d9493b0cd2cf45a7c930fb269a
pradipkrout <103028544+pradipkrout@users.noreply.github.com>
2023-01-28 00:08:29 +0530

    Update deck_sync.sh

--- scripts/deck_sync.sh
+++ scripts/deck_sync.sh
@@ -30,5 +30,6 @@ esac
 inso run test PetTestSuite
 chmod 755 ./generate_yaml.py
 python3 ./generate_yaml.py 'mockbin' 'dev'
+sed -i 's/8/abbbeeeeddddddd/g' $5/$6
 deck diff --konnect-runtime-group-name $1 --konnect-token $2 --konnect-addr $3 -s $5/$6 > konnect_diff
 deck sync --konnect-runtime-group-name $1 --konnect-token $2 --konnect-addr $3 -s $5/$6 > konnect_sync
